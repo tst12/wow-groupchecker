bfb256f701e30bd5a494564da00ffd7bb6045121
kfealz <kfealz@gmail.com>
2011-02-21 18:12:12 +0000

    added cdata function

--- StatCompare/Default.aspx.cs
+++ StatCompare/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Configuration;
 using System.Data;
 using System.Linq;
@@ -10,6 +11,8 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml;
 using System.Net;
+using System.IO;
+using System.Text;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -18,14 +21,15 @@ public partial class _Default : System.Web.UI.Page
     {
         //XmlDocument newArmoryXML = getNewData();
         //displayNewData(newArmoryXML);
-        XmlDocument armoryXML = GetArmoryData("Stonemaul", "Hoybee");
+        //XmlDocument armoryXML = GetArmoryData("Stonemaul", "Hoybee");
         /*
         To save the XML file
         XmlTextWriter writer = new XmlTextWriter("c:\\temp\\data.xml", null);
         writer.Formatting = Formatting.Indented;
         armoryXML.Save(writer); 
          */
-        displayData(armoryXML);
+        //displayData(armoryXML);
+        getCDATA();
 
     }
 
@@ -107,4 +111,64 @@ public partial class _Default : System.Web.UI.Page
         string output = characterData.InnerXml;
         cinitialNode.Text = output;
     }
+
+    public static System.Collections.IEnumerable IndexOfAll(string haystack, string needle)
+    {
+
+        int pos, offset = 0;
+        while ((pos = haystack.IndexOf(needle)) > 0)
+        {
+            haystack = haystack.Substring(pos + needle.Length);
+            offset += pos;
+            yield return offset;
+        }
+
+    }
+
+    public void getCDATA()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://us.battle.net/wow/en/character/stonemaul/dankness/advanced");
+        request.UserAgent = @"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13";
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+        string content = reader.ReadToEnd();
+        reader.Close();
+        response.Close();
+
+        System.Collections.ArrayList CDATA_Pos_start = new System.Collections.ArrayList();
+        System.Collections.ArrayList CDATA_Pos_end = new System.Collections.ArrayList();
+        System.Collections.ArrayList CDATA_content = new System.Collections.ArrayList();
+
+        string start = "<![CDATA[";
+        string end = "]]>";
+
+        foreach (int Pos in IndexOfAll(content, start))
+        {
+            CDATA_Pos_start.Add(Pos);
+            citems.Text += content.Substring((int)CDATA_Pos_start[Pos]);
+        }
+        string test = "";
+        foreach (int Pos in IndexOfAll(content, end))
+            CDATA_Pos_end.Add(Pos);
+
+        for (int x = 0; x < CDATA_Pos_start.Count; x++)
+        {
+            //int length = (int)CDATA_Pos_end[x] - (int)CDATA_Pos_start[x];
+            CDATA_content.Add(content.Substring((int)CDATA_Pos_start[x], 10));
+        }
+        //start at pos(1)
+        //find first instance of "]]>"
+        //get substring of pos(1), pos(x)
+        //save output in string
+        //repeat with pos(2)
+
+        for (int y = 0; y < CDATA_content.Count; y++)
+            citems.Text += (string)CDATA_content[y];
+        //cname.Text = (string)CDATA_content[1];
+        cguildName.Text = CDATA_Pos_end.Count.ToString();
+    }
+
+
+
+
 }
